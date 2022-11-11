2072c4665d041df2f4253b138d319217018b1892
401 <401@academicos.cesde.edu.co>
2022-11-11 11:11:22 -0500

    ok

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,7 +19,7 @@ public class Main {
         System.out.println("**************************************");
 
         //PEDIR LOS DATOS DE LA FIESTA:
-        System.out.println("Datos de la fiesta: ");
+        /*System.out.println("Datos de la fiesta: ");
         System.out.println("**************************************\n");
         System.out.print("Fecha: ");
         objetoFiesta.setFecha(entradaPorTeclado.nextLine());
@@ -37,7 +37,25 @@ public class Main {
         System.out.print("Digita el aforo: ");
         objetoLugar.setAforo(entradaPorTeclado.nextInt());
         System.out.print("Digita la direccion: ");
-        objetoLugar.setDireccion(entradaPorTeclado.next());
+        objetoLugar.setDireccion(entradaPorTeclado.next());*/
+
+        System.out.println("CREANDO ARREGLOS");
+        // ARREGLOS PRIMITIVOS
+        int[] numeros=new int[10];
+        int cargar;
+        //SI QUIERO ACCEDER A UN PEDACITO DE MEMORIA
+        //DENTRO DEL ARREGLO DEBO USAR LA POSICION O INDICE
+        //mostrando lo que almaceno en el arreglo
+        //CON UN CICLO FOR YO PUEDO RECORRER UN ARREGLO
+        for(int i=0; i<numeros.length; i++){
+            System.out.println("ingrese un numero: ");
+            numeros[i]=entradaPorTeclado.nextInt();
+        }
+
+        for(int i=0; i<numeros.length; i++){
+            System.out.println(numeros[i]);
+        }
+
 
 
 
